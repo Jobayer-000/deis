16ad21fc35f2dc0b724b12f61781f13c0c359174
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-01 08:58:27 +0600

    Update sampler.py

--- jax_deis/sampler.py
+++ jax_deis/sampler.py
@@ -27,9 +27,9 @@ def get_sampler_t_ab(sde, eps_fn, ts_phase, ts_order, num_step, ab_order):
     print(eps_coef)
     ab_coef = jnp.concatenate([x_coef[:, None], eps_coef], axis=1)
     print(ab_coef)
-    def sampler(x0,up_lr):
+    def sampler(x0, up_lr):
         def ab_body_fn(i, val):
-            x, up_lr, eps_pred = val
+            x, eps_pred = val
             s_t= rev_ts[i]
             
             #new_eps = eps_fn(x, up_lr)
@@ -39,7 +39,7 @@ def get_sampler_t_ab(sde, eps_fn, ts_phase, ts_order, num_step, ab_order):
 
 
         eps_pred = jnp.asarray([x0,] * ab_order)
-        img, _ = jax.lax.fori_loop(0, num_step, ab_body_fn, (x0, up_lr, eps_pred))
+        img, _ = jax.lax.fori_loop(0, num_step, ab_body_fn, (x0, eps_pred))
         return img
     return sampler
 
