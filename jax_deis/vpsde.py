63affbeeb45b9df31bff223e2b6ff0b8a5c6cbff
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-01 14:40:38 +0600

    Update vpsde.py

--- jax_deis/vpsde.py
+++ jax_deis/vpsde.py
@@ -89,7 +89,7 @@ def get_interp_fn(_xp, _fp):
 
       i = jnp.clip(jnp.searchsorted(xp, x, side='right'), 1, len(xp) - 1)
       print('i',i)
-      print('df',df)
+      print('fp',fp)
         
       df = fp[i] - fp[i - 1]
       dx = xp[i] - xp[i - 1]
