bf28ba2367bcedb49c1be2d3fce1cf4006ac74a4
Jobayer Hossain <76811632+Jobayer-000@users.noreply.github.com>
2022-09-02 11:50:17 +0600

    Update multistep.py

--- jax_deis/multistep.py
+++ jax_deis/multistep.py
@@ -42,6 +42,7 @@ def get_one_coef_per_step_fn(sde):
         integrand, t_inter, dt = _eps_coef_worker_fn(t_start, t_end, num_item)
         poly_coef = vec_poly_coef(t_inter, ts_poly, coef_idx)
         print('single_coef', poly_coef)
+        print('sum',jnp.sum(integrand * poly_coef))
         return jnp.sum(integrand * poly_coef) * dt
     return _worker
 
